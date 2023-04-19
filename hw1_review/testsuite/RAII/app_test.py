e340e1d4a540ecd23f009514bd036abb35f941c8
Olga Geta <114720484+OlgaGeta@users.noreply.github.com>
2023-04-20 00:15:11 +0300

    оригинальные интеграционные тесты

--- hw1_review/testsuite/RAII/app_test.py
+++ hw1_review/testsuite/RAII/app_test.py
@@ -26,6 +26,29 @@ def test_write_and_read_text(language, text):
     # Output success message
     print(f'Test for writing and checking text in {language} passed successfully.')
 
+#original
+def test_write_and_read_text_original(language, text):
+    
+    input_lines = [
+        'file.txt',
+        'w',
+        text,
+        'y',
+        'file.txt',
+        'r'
+    ]
+
+    
+    process = subprocess.Popen(['bin/main_RAII'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate('\n'.join(input_lines).encode())
+
+    
+    with open('file.txt', 'r', encoding='utf-8') as f:
+        assert f.read() == text
+
+   
+    print(f'Test for writing and reading text in {language} passed successfully.')
+
 # Define the test cases
 test_cases = [
     ('English', 'This is a test text'),
@@ -38,5 +61,7 @@ test_cases = [
 # Run the tests
 for language, text in test_cases:
     test_write_and_read_text(language, text)
+    test_write_and_read_text_original(language, text)
+
 
 
